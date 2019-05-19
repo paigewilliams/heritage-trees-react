c96ec48dd7bc50f7fbbe0a5db9de42407a9fc97d
Paige Williams <paw145@humboldt.edu>
2019-05-18 23:10:56 -0700

    Remove old google maps code.

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import styled from 'styled-components';
 // import mapboxgl from 'mapbox-gl';
 import ReactMapGL, { Marker } from 'react-map-gl';
@@ -41,7 +40,6 @@ class Map extends Component {
     );
   }
 
-
   render() {
     const { viewport } = this.state;
     return (
@@ -63,69 +61,9 @@ class Map extends Component {
   }
 }
 
-// class MapContainer extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       showingInfoWindow: false,
-//       activeMarker: {},
-//       selectedPlace: {}
-//     };
-//     this.onMarkerClick = this.onMarkerClick.bind(this);
-//   }
-
-//   onMarkerClick(props, marker) {
-//     this.setState({
-//       selectedPlace: props,
-//       activeMarker: marker,
-//       showingInfoWindow: true
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <Map
-//         google={this.props.google}
-//         style={style}
-//         initialCenter={{
-//           lat: 45.5122,
-//           lng: -122.6587
-//         }}
-//         zoom={12}>
-//         {Object.keys(this.props.treeData).map((treeId) => {
-//           let marker = this.props.treeData[treeId];
-//           return <Marker title={marker.properties.COMMON}
-//             position={{ lat: marker.properties.LAT, lng: marker.properties.LON }}
-//             address={marker.properties.SITE_ADDRESS}
-//             year={marker.properties.YEAR_Designated}
-//             onClick={this.onMarkerClick}
-//             key={treeId} />;
-//         }
-//         )}
-//         <InfoWindow
-//           marker={this.state.activeMarker}
-//           visible={this.state.showingInfoWindow}
-//         >
-//           <div>
-//             <h3>{this.state.selectedPlace.title}</h3>
-//             <p><strong>Address:</strong> {this.state.selectedPlace.address}</p>
-//             <p><strong>Year designated:</strong> {this.state.selectedPlace.year}</p>
-//           </div>
-//         </InfoWindow>
-//       </Map>
-//     );
-//   }
-// }
-
-
 Map.propTypes = {
   treeData: PropTypes.object
 };
 
 export default Map;
 
-// export default GoogleApiWrapper({
-//   /* eslint-disable no-undef */
-//   apiKey: (process.env.GOOGLE_MAPS_API)
-//   /* eslint-enable no-undef */
-// })(MapContainer);
