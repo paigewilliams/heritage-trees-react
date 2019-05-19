5da411f0fcba761f0246e187bb9c586191b2d05b
Paige Williams <paw145@humboldt.edu>
2019-05-18 19:26:22 -0700

    Begin converting map to include react-map-gl

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,76 +1,89 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 // import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import styled from 'styled-components';
-import mapboxgl from 'mapbox-gl';
-import v4 from 'uuid/v4';
+// import mapboxgl from 'mapbox-gl';
+import MapGL from 'react-map-gl';
+// import v4 from 'uuid/v4';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
-mapboxgl.accessToken = "pk.eyJ1IjoicGFpZ2V3aWxsaWFtcyIsImEiOiJjanNzNDlkc2QxcDg4NDRvMnllMTY5d3FhIn0.fdKpE8Pe2MuB4oe05YCvLw";
+const TOKEN = 'pk.eyJ1IjoicGFpZ2V3aWxsaWFtcyIsImEiOiJjanNzNDlkc2QxcDg4NDRvMnllMTY5d3FhIn0.fdKpE8Pe2MuB4oe05YCvLw';
 
 const MapContainerStyle = styled.div`
   width: 500px;
   height: 500px;
   position: absolute;
-`
+`;
 
-class Map extends React.Component {
-  map;
+class Map extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      lat: 45.5122,
-      lng: -122.6587,
-      zoom: 11
+      viewport: {
+        latitude: 45.5122,
+        longitude: -122.6587,
+        zoom: 11,
+        bearing: 0,
+        pitch: 0,
+        width: 500,
+        height: 500,
+      }
     };
   }
 
-  componentDidMount() {
-    const { lat, lng, zoom } = this.state;
-    this.map = new mapboxgl.Map({
-      container: this.mapContainer,
-      style: 'mapbox://styles/mapbox/light-v9',
-      center: [lng, lat],
-      zoom
-    });
-    this.map.on('move', () => {
-      const { lng, lat } = this.map.getCenter();
-      this.setState({
-        lng: lng.toFixed(4),
-        lat: lat.toFixed(4),
-        zoom: this.map.getZoom().toFixed(2)
-      });
-    });
-  }
+  // componentDidMount() {
+  //   const { lat, lng, zoom } = this.state;
+  //   this.map = new mapboxgl.Map({
+  //     container: this.mapContainer,
+  //     style: 'mapbox://styles/mapbox/light-v9',
+  //     center: [lng, lat],
+  //     zoom
+  //   });
+  //   this.map.on('move', () => {
+  //     const { lng, lat } = this.map.getCenter();
+  //     this.setState({
+  //       lng: lng.toFixed(4),
+  //       lat: lat.toFixed(4),
+  //       zoom: this.map.getZoom().toFixed(2)
+  //     });
+  //   });
+  // }
 
-  componentDidUpdate() {
-    console.log(this.props.treeData);
-    Object.keys(this.props.treeData).map(treeId => {
-      let tree = this.props.treeData[treeId]
-      this.map.addLayer({
-        'id': v4(),
-        'type': 'circle',
-        'source': {
-          'type': 'geojson',
-          'data': {
-            'type': 'Feature',
-            'properties': {
-              'address': tree.properties.SITE_ADDRESS,
-            },
-            'geometry': {
-              'type': 'Point',
-              'coordinates': tree.geometry.coordinates,
-            }
-          }
-        }
-      });
-    })
-  }
+  // componentDidUpdate() {
+  //   console.log(this.props.treeData);
+  //   Object.keys(this.props.treeData).map(treeId => {
+  //     let tree = this.props.treeData[treeId]
+  //     this.map.addLayer({
+  //       'id': v4(),
+  //       'type': 'circle',
+  //       'source': {
+  //         'type': 'geojson',
+  //         'data': {
+  //           'type': 'Feature',
+  //           'properties': {
+  //             'address': tree.properties.SITE_ADDRESS,
+  //           },
+  //           'geometry': {
+  //             'type': 'Point',
+  //             'coordinates': tree.geometry.coordinates,
+  //           }
+  //         }
+  //       }
+  //     });
+  //   })
+  // }
 
   render() {
+    const { viewport } = this.state;
     return (
-      <div>
-        <MapContainerStyle ref={el => this.mapContainer = el} />
-      </div>
+      <MapContainerStyle>
+        <MapGL
+          {...viewport}
+          mapStyle='mapbox://styles/mapbox/light-v9'
+          mapboxApiAccessToken={TOKEN}
+          onViewportChange={(viewport) => this.setState({ viewport })}>
+        </MapGL>
+      </MapContainerStyle>
     );
   }
 }
