348643f1f0408d5e75b3dec328cca9146bfd35d2
Paige Williams <paw145@humboldt.edu>
2019-06-02 15:48:46 -0700

    Render barChart; currently cut off

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -5,7 +5,7 @@ import { max } from 'd3-array';
 import { select } from 'd3-selection';
 
 class BarChart extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.createBarChart = this.createBarChart.bind(this);
   }
@@ -21,12 +21,40 @@ class BarChart extends Component {
   createBarChart() {
     const node = this.node;
     const { data } = this.props;
-    console.log(data);
+    const treeHeights = Object.keys(data).map(id => {
+      const height = data[id].properties.HEIGHT;
+      return height;
+    });
+    const dataMax = max(treeHeights);
+    const yScale = scaleLinear()
+      .domain([0, dataMax])
+      .range([0, 500])
+
+    select(node)
+      .selectAll('rect')
+      .data(treeHeights)
+      .enter()
+      .append('rect')
+
+    select(node)
+      .selectAll('rect')
+      .data(treeHeights)
+      .exit()
+      .remove()
+
+    select(node)
+      .selectAll('rect')
+      .data(treeHeights)
+      .style('fill', '#fe9922')
+      .attr('x', (d, i) => i * 25)
+      .attr('y', d => 500 - yScale(d))
+      .attr('height', d => yScale(d))
+      .attr('width', 25)
   }
 
   render() {
-    return <svg ref={node => this.node}
-    width={1000} height={500}></svg>
+    return <svg ref={node => this.node = node}
+      width={5000} height={500}></svg>
   }
 
 }
