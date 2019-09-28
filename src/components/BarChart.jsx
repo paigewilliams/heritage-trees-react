7261652f3cd4736cfa3192fd2a439a2b84572276
Paige Williams <paw145@humboldt.edu>
2019-09-28 16:20:59 -0700

    convert barchart to hooks

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,48 +1,37 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { scaleLinear } from 'd3-scale';
 import { max } from 'd3-array';
 import { select } from 'd3-selection';
 
-class BarChart extends Component {
-  constructor(props) {
-    super(props);
-    this.createBarChart = this.createBarChart.bind(this);
-  }
+const BarChart = ({data}) => {
+  const ref = useRef(null);
 
-  componentDidMount() {
-    this.createBarChart();
-  }
+  useEffect(() => {
+    createBarChart();
+  }, [data]);
 
-  componentDidUpdate() {
-    this.createBarChart();
-  }
-
-  createBarChart() {
-    const node = this.node;
-    const { data } = this.props;
-    const treeHeights = Object.keys(data).map(id => {
-      const height = data[id].properties.HEIGHT;
-      return height;
-    });
+  const createBarChart = () => {
+    const treeHeights = Object.keys(data).map(id => data[id].properties.HEIGHT);
     const dataMax = max(treeHeights);
     const yScale = scaleLinear()
       .domain([0, dataMax])
       .range([0, 300]);
 
-    select(node)
+    select(ref.current)
       .selectAll('rect')
       .data(treeHeights)
       .enter()
       .append('rect');
 
-    select(node)
+    select(ref.current)
       .selectAll('rect')
       .data(treeHeights)
       .exit()
       .remove();
 
-    select(node)
+    select(ref.current)
       .selectAll('rect')
       .data(treeHeights)
       .style('fill', '#fe9922')
@@ -50,19 +39,21 @@ class BarChart extends Component {
       .attr('y', d => 500 - yScale(d))
       .attr('height', d => yScale(d))
       .attr('width', 3);
-  }
+  };
 
-  render() {
-    return (
-      <svg ref={node => (this.node = node)} width={2000} height={500}></svg>
-    );
-  }
-}
+  return (
+    <svg ref={ref} width={2000} height={500}></svg>
+  );
+};
 
 const mapStateToProps = state => {
   return {
     data: state.treeData
   };
+};
+
+BarChart.propTypes = {
+  data: PropTypes.object.isRequired
 };
 
 export default connect(mapStateToProps)(BarChart);
