42069840839995124a57f60352e4c5c02ad7b66e
Paige Williams <paw145@humboldt.edu>
2019-10-14 11:45:59 -0700

    remove redux structure

--- src/constants/ActionTypes.js
+++
@@ -1,2 +0,0 @@
-export const REQUEST_DATA = 'REQUEST_DATA';
-export const FILTER_DATA = 'FILTER_DATA';
--- src/middleware/persist-data-locally.js
+++
@@ -1,6 +0,0 @@
-const persistDataLocally = store => next => action => {
-  next(action);
-  localStorage['reduxStore'] = JSON.stringify(store.getState());
-};
-
-export default persistDataLocally;
